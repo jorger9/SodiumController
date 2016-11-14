e5a8d6eb07a9d273240d5016051518dc3abac44b
Jorge Alberto Gómez Mendoza <jagomezmendoza@gmail.com>
2016-11-13 22:58:18 -0600

    fragments names changed

--- app/src/main/java/com/jorger9/sodiumcontroller/view/fragment/HomeFragment.java
+++ app/src/main/java/com/jorger9/sodiumcontroller/view/fragment/HomeFragment.java
@@ -12,10 +12,10 @@ import com.jorger9.sodiumcontroller.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StadisticsFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
 
-    public StadisticsFragment() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -24,7 +24,7 @@ public class StadisticsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stadistics, container, false);
+        return inflater.inflate(R.layout.fragment_eat, container, false);
     }
 
 }
